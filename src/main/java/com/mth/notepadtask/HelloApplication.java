12190ca1446c2cd58aaed7be093028c9fb3124d9
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-12 21:51:10 +0100

    Change REST API endpoint path to /api/v1/notepad.

--- src/main/java/com/mth/notepadtask/HelloApplication.java
+++ src/main/java/com/mth/notepadtask/HelloApplication.java
@@ -3,7 +3,7 @@ package com.mth.notepadtask;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/api/v1/notepad")
 public class HelloApplication extends Application {
 
 }
