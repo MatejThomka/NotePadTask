c2459701a4bada69c71c44a187b9de3ec26c1d62
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-13 04:39:14 +0100

    Create TaskService interface with methods, it is just test.

--- src/main/java/com/mth/notepadtask/service/TaskService.java
+++ src/main/java/com/mth/notepadtask/service/TaskService.java
@@ -1,4 +1,18 @@
 package com.mth.notepadtask.service;
 
+
+import com.mth.notepadtask.entity.Task;
+import jakarta.ejb.Local;
+import java.util.List;
+
+@Local
 public interface TaskService {
+
+  Task createTask(Task task);
+  Task updateTask(Long id, Task task);
+  void deleteTask(Long id);
+  Task getTask(Long id);
+  List<Task> getAllTask();
+
+
 }
