5b2a26017aecbc2d2e127bd1e6efdbb078e93033
Matej Thomka <matej.thomka.eleinst@gmail.com>
2023-12-16 08:53:35 +0100

    Change updateTask to void.

--- src/main/java/com/mth/notepadtask/dao/TaskDao.java
+++ src/main/java/com/mth/notepadtask/dao/TaskDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface TaskDao {
 
   void createTask(Task task) throws TaskException;
-  Task updateTask(Long id, Task task);
+  void updateTask(Task task);
   void deleteTask(Long id);
   Task getTask(Long id);
   List<Task> getAllTask();
